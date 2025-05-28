74278b5020bb180cd9473c60dcc8abf0c3bd1005
kjkkmmmmjj <mictesting1234hello@gmail.com>
2025-05-28 22:18:27 +0545

    Fix Firebase Auth Configuration

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,34 +1,29 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import Constants from 'expo-constants';
+import { initializeAuth, getAuth, getReactNativePersistence } from 'firebase/auth';
 import { Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAVYuEswLiTZD_tpV_7vQgED9RnK3Cy-QE",
-  authDomain: "hisab-kitab-ad197.firebaseapp.com",
-  projectId: "hisab-kitab-ad197",
-  storageBucket: "hisab-kitab-ad197.firebasestorage.app",
-  messagingSenderId: "838491649019",
-  appId: "1:838491649019:web:16db04c04280fd5c2a915e",
-  measurementId: "G-PKPBDH24BR"
+  apiKey: Constants.expoConfig?.extra?.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig?.extra?.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig?.extra?.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig?.extra?.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig?.extra?.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig?.extra?.FIREBASE_APP_ID,
+  measurementId: Constants.expoConfig?.extra?.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth
-let auth;
-if (Platform.OS === 'web') {
-  auth = getAuth(app);
-} else {
-  const { initializeAuth, getReactNativePersistence } = require('firebase/auth');
-  auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
-  });
-}
+// Initialize Auth with persistence
+const auth = Platform.OS === 'web' 
+  ? getAuth(app)
+  : initializeAuth(app, {
+      persistence: getReactNativePersistence(AsyncStorage)
+    });
 
 // Initialize Firestore
 const db = getFirestore(app);
